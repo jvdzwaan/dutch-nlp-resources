6d8481c681f02f98d47c831a460cb39cef9f6908
Janneke van der Zwaan <jannekevdzwaan@gmail.com>
2020-02-17 22:32:21 +0100

    Complete adding of rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -28,6 +29,7 @@ author = 'Janneke van der Zwaan'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
